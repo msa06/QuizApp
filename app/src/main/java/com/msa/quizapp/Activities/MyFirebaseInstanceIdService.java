f9c3d14aad259d4446eb247d7ccf88fb569a18ff
MinoliVartak <36539574+MinoliVartak@users.noreply.github.com>
2019-02-01 02:33:32 +0530

    some minor changes

--- app/src/main/java/com/msa/quizapp/Activities/MyFirebaseInstanceIdService.java
+++ app/src/main/java/com/msa/quizapp/Activities/MyFirebaseInstanceIdService.java
@@ -1,20 +1,38 @@
 package com.msa.quizapp.Activities;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.msa.quizapp.R;
 
-public class MyFirebaseInstanceIdService extends FirebaseMessagingService {
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
 
 
     private static final String TAG = "MyFirebaseIIDService";
-    
-    @Override
-    public void onNewToken(String s) {
-        // Get updated InstanceID token.
 
-        super.onNewToken(s);
-        Log.d("NEW_TOKEN", s);
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
     }
 
     private void sendRegistrationToServer(String token) {
